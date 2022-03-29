920b4ba58cb7fabad48f9590f0861b9ef816c46f
Michi1090 <michi.m1090@gmail.com>
2022-03-29 16:55:42 +0900

    ホームメニュー画面実装 #6

--- src/laravel_project/app/Http/Controllers/HomeController.php
+++ src/laravel_project/app/Http/Controllers/HomeController.php
@@ -13,6 +13,17 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $menus = [
+            ['title' => '生産登録', 'url' => '#'],
+            ['title' => '出荷登録', 'url' => '#'],
+            ['title' => '在庫照会', 'url' => '#'],
+            ['title' => '部品発注', 'url' => '#'],
+            ['title' => '製品マスタ照会', 'url' => '#'],
+            ['title' => '部品マスタ照会', 'url' => '#'],
+            ['title' => 'ユーザー情報照会・変更', 'url' => '#'],
+            ['title' => '操作マニュアル', 'url' => '#'],
+        ];
+
+        return view('home', ['menus' => $menus]);
     }
 }
