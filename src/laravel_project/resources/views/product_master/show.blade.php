c05593bdc8e60f6796be4defeec4d39b10e0beda
Michi1090 <michi.m1090@gmail.com>
2022-04-14 23:34:33 +0900

    製品マスタから部品マスタへのリンク設定 #15

--- src/laravel_project/resources/views/product_master/show.blade.php
+++ src/laravel_project/resources/views/product_master/show.blade.php
@@ -22,19 +22,31 @@
                             <tbody>
                                 <tr>
                                     <th scope="row">1</th>
-                                    <td>{{ $product->engine->name }}</td>
+                                    <td>
+                                        <a href="{{ route('material_master', ['id' => $product->engine_id]) }}" target="_blank">
+                                            {{ $product->engine->name }}
+                                        </a>
+                                    </td>
                                     <td>{{ $product->engine_per }} pcs</td>
                                     <td>エンジン</td>
                                 </tr>
                                 <tr>
                                     <th scope="row">2</th>
-                                    <td>{{ $product->airbag->name }}</td>
+                                    <td>
+                                        <a href="{{ route('material_master', ['id' => $product->airbag_id]) }}" target="_blank">
+                                            {{ $product->airbag->name }}
+                                        </a>
+                                    </td>
                                     <td>{{ $product->airbag_per }} pcs</td>
                                     <td>エアバッグ</td>
                                 </tr>
                                 <tr class="border-bottom">
                                     <th scope="row">3</th>
-                                    <td>{{ $product->tire->name }}</td>
+                                    <td>
+                                        <a href="{{ route('material_master', ['id' => $product->tire_id]) }}" target="_blank">
+                                            {{ $product->tire->name }}
+                                        </a>
+                                    </td>
                                     <td>{{ $product->tire_per }} pcs</td>
                                     <td>タイヤ</td>
                                 </tr>
