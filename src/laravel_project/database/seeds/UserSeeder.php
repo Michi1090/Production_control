49047bb2cf8c5da9d43177ad0f18438acf0a8194
Michi1090 <michi.m1090@gmail.com>
2022-04-22 16:39:31 +0900

    UserSeederをFakerでリファクタ #20

--- src/laravel_project/database/seeds/UserSeeder.php
+++ src/laravel_project/database/seeds/UserSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Faker\Factory;
+use App\User;
 use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
@@ -13,69 +15,15 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $users = [
-            [
-                'name' => 'test1',
-                'email' => 'test1@gmail.com',
-                'password' => Hash::make('123456Aa'),
-                'created_at' => new DateTime(),
-            ],
-            [
-                'name' => 'test2',
-                'email' => 'test2@gmail.com',
-                'password' => Hash::make('123456Aa'),
-                'created_at' => new DateTime(),
-            ],
-            [
-                'name' => 'test3',
-                'email' => 'test3@gmail.com',
-                'password' => Hash::make('123456Aa'),
-                'created_at' => new DateTime(),
-            ],
-            [
-                'name' => 'test4',
-                'email' => 'test4@gmail.com',
-                'password' => Hash::make('123456Aa'),
-                'created_at' => new DateTime(),
-            ],
-            [
-                'name' => 'test5',
-                'email' => 'test5@gmail.com',
-                'password' => Hash::make('123456Aa'),
-                'created_at' => new DateTime(),
-            ],
-            [
-                'name' => 'test6',
-                'email' => 'test6@gmail.com',
-                'password' => Hash::make('123456Aa'),
-                'created_at' => new DateTime(),
-            ],
-            [
-                'name' => 'test7',
-                'email' => 'test7@gmail.com',
-                'password' => Hash::make('123456Aa'),
-                'created_at' => new DateTime(),
-            ],
-            [
-                'name' => 'test8',
-                'email' => 'test8@gmail.com',
-                'password' => Hash::make('123456Aa'),
-                'created_at' => new DateTime(),
-            ],
-            [
-                'name' => 'test9',
-                'email' => 'test9@gmail.com',
-                'password' => Hash::make('123456Aa'),
-                'created_at' => new DateTime(),
-            ],
-            [
-                'name' => 'test10',
-                'email' => 'test10@gmail.com',
-                'password' => Hash::make('123456Aa'),
-                'created_at' => new DateTime(),
-            ],
-        ];
+        DB::table('users')->delete();
+        $faker = Factory::create('ja_JP');
 
-        DB::table('users')->insert($users);
+        for ($i = 0; $i < 10; $i++) {
+            User::create([
+                'name' => $faker->name(),
+                'email' => $faker->email(),
+                'password' => Hash::make('123456Aa'),
+            ]);
+        }
     }
 }
